a39a4bfc3911f3589dc16317b629c11e65293b45
hiiru <hiiru@rulez.ch>
2016-02-26 22:21:20 +0100

    initial proof of concept commit

---
+++ BenchmarkDotNet-bug-paramsfloat/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkDotNet_bug_paramsfloat
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BugPoC>();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BenchmarkDotNet-bug-paramsfloat/bugPoC.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+
+namespace BenchmarkDotNet_bug_paramsfloat
+{
+    [Config(typeof(Config))]
+    public class BugPoC
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(Job.RyuJitX64.With(Mode.Throughput).WithLaunchCount(1).WithWarmupCount(1).WithTargetCount(1));
+            }
+        }
+
+        [Params(0.2f, 0.8f, 0f, 0.546532f)]
+        public float Bugged { get; set; }
+
+        [Benchmark]
+        public float ReturnParam()
+        {
+            return Bugged;
+        }
+
+        [Benchmark]
+        public float ReturnParam2()
+        {
+            return Bugged;
+        }
+    }
+}
